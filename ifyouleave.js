71ff79ddecf15bef01e1f7176e41743f030ad694
cupofdirtfordinner <75740323+cupofdirtfordinner@users.noreply.github.com>
2021-04-21 10:02:52 -0500

    Update ifyouleave.js

--- ifyouleave.js
+++ ifyouleave.js
@@ -1,5 +1,6 @@
 window.addEventListener("beforeunload", function (e) {
     var confirmationMessage = 'It looks like you have been editing something or not idk. '
+    window.open("https://www.dirtcool.tk");
     (e || window.event).returnValue = confirmationMessage; //Gecko + IE
     return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
 }); 
